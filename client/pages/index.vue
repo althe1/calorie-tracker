b6985340766c1fd3e128d8f15d8166b37f034cd1
Alfred Kirakosian <gituser@tangosend.com>
2021-03-12 23:23:41 -0800

    Add calories to graph

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="container">
-   <h1>Main Page</h1>
-  </div>
+  <b-container>
+    <h1>Main Page</h1>
+    <b-row>
+      <b-col>
+        <line-chart v-if="data" :data="data" />
+      </b-col>
+      <b-col>
+        <calorie-form v-on:addValue="addValue" />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
-  middleware: "auth"
-}
+  middleware: "auth",
+  data() {
+    return {
+      data: null
+    };
+  },
+  async mounted() {
+    this.data = await this.$axios.$get("/calories");
+  },
+  methods: {
+    addValue(entry) {
+      this.data.push(entry);
+      console.log({ entry });
+    }
+  }
+};
 </script>
